e3a43dede997adf3a04d243d55d232acb72a7b99
Atfaster <atfasters@gmail.com>
2026-03-27 18:44:00 +0500

    Батник с автозапуском браузера

--- start_local_map_server.bat
+++ start_local_map_server.bat
@@ -5,6 +5,7 @@ cd /d "%~dp0"
 
 echo Starting Armies Of Terra local map server...
 echo.
+start "" powershell -NoProfile -Command "Start-Sleep -Seconds 1; Start-Process 'http://127.0.0.1:8000/'"
 python scripts\local_map_server.py
 
 echo.
